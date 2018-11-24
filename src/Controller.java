27ff21196c4e0e4d505dcb5e0a9cf87b6484eca7
nkokkirala <43920690+nkokkirala@users.noreply.github.com>
2018-11-23 22:00:07 -0500

    Add files via upload

---
+++ src/Controller.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package movieproject;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.EventListener;
+
+
+public class Controller{
+    
+    private View1 views;
+
+    Controller (Model model, View1 view)
+    {
+        views = view;
+        views.addALSeats(new seatListener());
+        
+    }
+    
+    class seatListener implements ActionListener {                                        
+        public void actionPerformed(ActionEvent e) {
+            views.checkout();
+        }      
+    }  
+}
+
---
+++ src/Csc4380Project3.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package movieproject;
+
+import java.io.IOException;
+
+
+public class Csc4380Project3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException {
+        
+        Model model = new Model();
+        View1 view = new View1 (model);
+        Controller controller = new Controller (model, view);
+        view.setVisible(true);
+        
+    }
+    
+}
